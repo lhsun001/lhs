06d5ecdf290b7f96a735380623d81c5ab6e9bd7c
User <User@User-PC>
2017-10-18 14:09:02 +0900

    나는 안바쁜데

--- 20171018GIT2/src/Main.java
+++ 20171018GIT2/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("¾È³ç");
 		System.out.println("¿äÁò ¹¹ÇØ ?");
-		
+		System.out.println("³ª´Â ¾È¹Ù»Ûµ¥");
 
 	}
 
